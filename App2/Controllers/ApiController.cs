c16845d1dd945c238c2b2a043c1d912cbdea8950
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2022-10-15 19:30:39 -0300

    End Point para listar jobs

--- App2/Controllers/ApiController.cs
+++ App2/Controllers/ApiController.cs
@@ -1,5 +1,7 @@
-﻿using App2.Context;
+﻿using System.Linq;
 using App2.Models;
+using System.Data;
+using App2.Context;
 using App2.Models.Repositores;
 using Microsoft.AspNetCore.Mvc;
 
@@ -10,22 +12,29 @@ namespace App2.Controllers
     public class ApiController : ControllerBase
     {
         protected readonly EmployeesRepositore _employerRepo;
+        protected readonly JobHistoryRepositore _jobHistoryRepositore;
 
         public ApiController(ContextDb context)
         {
             _employerRepo = new EmployeesRepositore(context);
+            _jobHistoryRepositore = new JobHistoryRepositore(context);
         }
         [HttpGet]
         public string index()
         {
-            
-            return "123";
+            return "123\n";
         }
 
         [HttpGet("employees")]
         public async Task<IList<EmployeesModel>> employes()
         {
             return await _employerRepo.GetEmployes();
+        }
+
+        [HttpGet("getjobs")]
+        public async Task<IList<JobHistoryModel>> GetJobs()
+        {
+            return await _jobHistoryRepositore.GetJobs();
         }
     }
 }
