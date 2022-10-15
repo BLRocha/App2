a705d0c2afd029df84b9dbf7b3c9d264ea50b037
Leandro Rocha <leandrorocha.bezerra@gmail.com>
2022-10-15 19:35:05 -0300

    Abstarct Context Repository

---
+++ App2/Models/Repositores/Repository/ARepository.cs
@@ -0,0 +1,14 @@
+﻿using App2.Context;
+
+namespace App2.Models.Repositores.Repository
+{
+    abstract public class ARepository
+    {
+        protected readonly ContextDb _context;
+
+        public ARepository(ContextDb context)
+        {
+            _context = context;
+        }
+    }
+}
